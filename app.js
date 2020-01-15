eb364d5ffafef0527240e4be7ec07af4f4da981a
Ahuoiza <oizabaiye@gmail.com>
2020-01-15 17:45:06 -0500

    put in hamburger menu

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ $(document).ready(function() {
         $(this).next().toggle(600)
     })
 })
+
+// mobile menu functionality
+const openMenu = () => {
+    document.getElementById('sideMenu').style.width = '100vw';
+  }
+  
+  //? on clicking the x, return to default
+  const closeMenu = () => {
+    document.getElementById('sideMenu').style.width = '0';
+  }
