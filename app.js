cae3ea60c3597d0f60e2cd7b07488250289fc34c
Ahuoiza <oizabaiye@gmail.com>
2020-01-16 18:00:42 -0500

    finalize

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ $(document).ready(function() {
     //keep about me text hidden
     $('.innerBox').hide()
 
+    // keeps 'JavaScript is my jam' active on page load
+    $('#inner1').show()
+    $('#makeActive').addClass('isActive')
+
     // on click of heading, show about me text
     $('h3').on('click', function(){
         $(this).next().toggle(600)
+
+        // check if plus or minus is in the DOM
+        let plusSign = $(this).find('.fa-plus')
+        let minusSign = $(this).find('.fa-minus')
+        console.log(plusSign)
+
+        //if there's a plus sign remove it and show minus, and vice versa
+        plusSign.toggleClass('hide')
+        minusSign.toggleClass('hide')
     })
+
+    $('#makeActive').on('click', function(){
+        $(this).toggleClass('isActive')
+    })
+
 })
+
 
 // mobile menu functionality
 const openMenu = () => {
